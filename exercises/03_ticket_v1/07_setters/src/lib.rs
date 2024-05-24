fd3db1f956aa3e490fa12d30e09169bc8969f2a7
Ricardo B. Marliere <ricardo@marliere.net>
2024-05-24 18:48:19 +0000

    03_ticket_v1/07_setters

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -10,30 +10,6 @@ pub struct Ticket {
 }
 
 impl Ticket {
-    pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
-
-        Ticket {
-            title,
-            description,
-            status,
-        }
-    }
-
     pub fn title(&self) -> &String {
         &self.title
     }
@@ -45,6 +21,46 @@ impl Ticket {
     pub fn status(&self) -> &String {
         &self.status
     }
+
+    pub fn set_title(&mut self, new_title: String) {
+        if new_title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if new_title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+        self.title = new_title;
+    }
+
+    pub fn set_description(&mut self, new_description: String) {
+        if new_description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if new_description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+        self.description = new_description;
+    }
+
+    pub fn set_status(&mut self, new_status: String) {
+        if new_status != "To-Do" && new_status != "In Progress" && new_status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+        self.status = new_status;
+    }
+
+    pub fn new(title: String, description: String, status: String) -> Ticket {
+        let mut ticket = Ticket {
+            title: String::new(),
+            description: String::new(),
+            status: String::new(),
+        };
+        ticket.set_title(title);
+        ticket.set_description(description);
+        ticket.set_status(status);
+        ticket
+    }
+
 }
 
 #[cfg(test)]
