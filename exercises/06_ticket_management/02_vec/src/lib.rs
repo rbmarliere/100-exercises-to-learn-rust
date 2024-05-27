c3a062fdc7628433a470db2b2175052599803182
Ricardo B. Marliere <ricardo@marliere.net>
2024-05-27 16:35:08 +0000

    06_ticket_management/02_vec

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -10,12 +10,23 @@
 //
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
+
 pub fn fibonacci(n: u32) -> u32 {
     // TODO: implement the `fibonacci` function
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut seq = vec![0, 1];
+
+    if n == 0 || n == 1 {
+        return seq[n as usize];
+    }
+
+    for i in 2..=n {
+        seq.push(seq[(i - 2) as usize] + seq[(i - 1) as usize])
+    }
+
+    seq[n as usize]
 }
 
 #[cfg(test)]
